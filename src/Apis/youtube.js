19291217810ccda55339a6b91a8481529c19898a
Yun-Hsuan Chu <jenniferchutw@gmail.com>
2020-02-25 20:16:28 +1100

    second

---
+++ src/Apis/youtube.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+const KEY = 'AIzaSyDpPm2gDksydIr8MqQdPtqnHqfgVsu1Eho';
+
+export default axios.create({
+  baseURL: 'https://www.googleapis.com/youtube/v3',
+  params: { part: 'snippet', type: 'video', maxResults: 5, key: KEY }
+});
